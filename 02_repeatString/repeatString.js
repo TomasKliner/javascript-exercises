3795789ac28d86a03b2d01fd7dad4a11f34ad971
Tomas Kliner <tomasklinerr@gmail.com>
2022-04-23 10:47:11 +0200

    Repeat String all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (slovo, cislo) {
+    if(cislo<0)return "ERROR"
+    let retezec = "";
+    for (let i = 0; i < cislo; ++i) retezec += slovo;
+    return retezec;
 };
 
 // Do not edit below this line
