50e0cc5560da86ce8b90787dff1f46af3f4b360e
Tomas Kliner <tomasklinerr@gmail.com>
2022-04-23 11:17:28 +0200

    removeFromArray all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...other_args) {
+    for (let j = 0; j < other_args.length; j++) {
+        for (var i = 0; i < arr.length; i++) {
+
+            if (arr[i] === other_args[j]) { //=== removes onlz same value and type of value as
+
+                arr.splice(i, 1);
+            }
+
+        }
+    }
+    return arr;
 
 };
 
