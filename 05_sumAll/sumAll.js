92e0698c102c6745d3210b8457cc1d7a4bc64c75
Tomas Kliner <tomasklinerr@gmail.com>
2022-04-23 11:25:39 +0200

    sumAll: all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const { isObjectExpression, isNumberLiteral } = require("@babel/types");
+
+const sumAll = function(a,b) {
+    if (a < 0 || b<0) return "ERROR";
+    if (typeof(a) != 'number' || typeof(b) != 'number') return "ERROR";
+    if(b<a){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let sum =0;
+    for(let i =a; i<=b; ++i )
+    {
+        sum+=i;
+    }
+    return sum;
 
 };
 
